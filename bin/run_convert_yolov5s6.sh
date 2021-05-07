5af7d6a935243c3349835613e054658dd87949f4
DuongTran1708 <duongtran.k10@gmail.com>
2021-05-07 16:52:24 +0900

    Update README.md

--- bin/run_convert_yolov5s6.sh
+++ bin/run_convert_yolov5s6.sh
@@ -3,6 +3,7 @@ weight_input_folder=tss/detector/yolov5/weights/yolov5s6_trt
 weight_output_folder=tss/detector/yolov5/weights/yolov5s6_trt
 build_folder=bin/tensorrtx/yolov5/build
 model=s6
+$build_folder/yolov5 -s $weight_input_folder/yolov5s6_aicity2021_full.wts $weight_output_folder/yolov5s6_aicity2021_full.engine $model
 $build_folder/yolov5 -s $weight_input_folder/yolov5s6_aicity2021_cam_1_full.wts $weight_output_folder/yolov5s6_aicity2021_cam_1_full.engine $model
 $build_folder/yolov5 -s $weight_input_folder/yolov5s6_aicity2021_cam_1_dawn_full.wts $weight_output_folder/yolov5s6_aicity2021_cam_1_dawn_full.engine $model
 $build_folder/yolov5 -s $weight_input_folder/yolov5s6_aicity2021_cam_1_rain_full.wts $weight_output_folder/yolov5s6_aicity2021_cam_1_rain_full.engine $model
