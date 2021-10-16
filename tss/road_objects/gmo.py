ba278a450ddd96c5d8d11d78496eca97e7782665
DuongTran1708 <duongtran.k10@gmail.com>
2021-10-16 12:35:30 +0900

    Update tss objects

--- tss/road_objects/gmo.py
+++ tss/road_objects/gmo.py
@@ -19,15 +19,12 @@ from tss.road_objects.moving_model import MovingState
 # MARK: - GMO (General Moving Object)
 
 class GMO(GeneralObject, MotionModel, MovingModel):
-	"""GMO (General Moving Object) = GeneralObject + MotionModel + CountingModel
-	"""
-	
 	# MARK: Class Property
 	
-	min_entering_distance: int = 0    # Min distance when an object enters the ROI to be Confirmed.
-	min_traveled_distance: int = 100  # Min distance between first trajectory point with last trajectory point.
-	min_hit_streak       : int = 10   # Min number of "consecutive"'" frame has that track appear.
-	max_age              : int = 1    # Max frame to wait until a dead track can be counted.
+	min_entering_distance: int = 0
+	min_traveled_distance: int = 100
+	min_hit_streak       : int = 10
+	max_age              : int = 1
 	
 	# MARK: Magic Functions
 	
@@ -40,8 +37,7 @@ class GMO(GeneralObject, MotionModel, MovingModel):
 	
 	@classmethod
 	def gmo_from_detection(cls, detection: Detection, **kwargs):
-		"""Create ``GMO`` object from ``Detection`` object.
-		"""
+
 		return cls(
 			frame_index = detection.frame_index,
 			bbox        = detection.bbox,
@@ -55,8 +51,6 @@ class GMO(GeneralObject, MotionModel, MovingModel):
 	# MARK: Update
 	
 	def update_gmo(self, detection: Detection):
-		"""Update the whole GMO object with newly matched ``Detection``.
-		"""
 		# TODO: First update ``GeneralObject``
 		self.update_go_from_detection(detection=detection)
 		
@@ -64,34 +58,25 @@ class GMO(GeneralObject, MotionModel, MovingModel):
 		self.update_motion_state()
 		
 	def update_moving_state(self, rois: List[ROI], **kwargs):
-		"""Update the current state of the road_objects.
-		"""
 		roi = next((roi for roi in rois if roi.uuid == self.roi_uuid), None)
 		if roi is None:
 			return
 		
-		# TODO: From Candidate --> Confirmed
 		if self.is_candidate:
 			if self.hit_streak >= GMO.min_hit_streak and \
 				roi.is_bbox_in_or_touch_roi(bbox_xyxy=self.current_bbox, compute_distance=True) >= GMO.min_entering_distance and \
 				self.travelled_distance >= GMO.min_traveled_distance:
 				self.moving_state = MovingState.Confirmed
 		
-		# TODO: From Confirmed --> Counting
 		elif self.is_confirmed:
 			if roi.is_bbox_in_or_touch_roi(bbox_xyxy=self.current_bbox) <= 0:
 				self.moving_state = MovingState.Counting
 			
-		# TODO: From Counting --> ToBeCounted
 		elif self.is_counting:
 			if roi.is_center_in_or_touch_roi(bbox_xyxy=self.current_bbox) < 0 or \
 				self.time_since_update >= GMO.max_age:
 				self.moving_state = MovingState.ToBeCounted
 			
-		# TODO: From ToBeCounted --> Counted
-		# Perform when counting the vehicle
-	
-		# TODO: From Counted --> Exiting
 		elif self.is_counted:
 			if roi.is_center_in_or_touch_roi(bbox_xyxy=self.current_bbox, compute_distance=True) <= 0 or \
 				self.time_since_update >= GMO.max_age:
@@ -100,8 +85,6 @@ class GMO(GeneralObject, MotionModel, MovingModel):
 	# MARK: Visualize
 
 	def draw(self, drawing, **kwargs):
-		"""Draw the object into the ``drawing`` based on the current moving_state.
-		"""
 		if self.is_confirmed:
 			GeneralObject.draw(self, drawing=drawing, label=False, **kwargs)
 		elif self.is_counting:
