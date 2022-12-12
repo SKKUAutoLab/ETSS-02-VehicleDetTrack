e4553adc9ae4f40f21629c390cc1d2ff6bcae0a8
sugarubuntu <duongtran.k10@gmail.com>
2022-12-12 18:58:07 +0900

    Update license.

--- tfe/camera/camera_multiprocess.py
+++ tfe/camera/camera_multiprocess.py
@@ -1,10 +1,19 @@
 # ==================================================================== #
-# File name: camera_multiprocess.py
-# Author: Long H. Pham and Duong N.-N. Tran
-# Date created: 03/27/2021
+# Copyright (C) 2022 - Automation Lab - Sungkyunkwan University
 #
-# ``Camera`` class contains all necessary modules and the main loop for
-# detection and tracking vehicles. It also counts and visualize results.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 # ==================================================================== #
 import os
 import threading
