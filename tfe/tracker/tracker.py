0a4b50df3d83cd2fc96f085d22355343c961dc99
sugarubuntu <duongtran.k10@gmail.com>
2022-12-12 19:09:02 +0900

    Update license.

--- tfe/tracker/tracker.py
+++ tfe/tracker/tracker.py
@@ -1,7 +1,19 @@
 # ==================================================================== #
-# File name: tracker.py
-# Author: Automation Lab - Sungkyunkwan University
-# Date created: 03/30/2021
+# Copyright (C) 2022 - Automation Lab - Sungkyunkwan University
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # ``Tracker`` base class for all variant of tracker.
 # It define a unify template to guarantee the input and output of all tracker are the same.
