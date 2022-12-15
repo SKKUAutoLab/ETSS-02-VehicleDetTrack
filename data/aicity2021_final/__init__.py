09087007e1b00dd4164635d36fa2b37c8b9d7b52
sugarubuntu <duongtran.k10@gmail.com>
2022-12-15 16:39:31 +0900

    Add config of videos.

--- data/aicity2021_final/__init__.py
+++ data/aicity2021_final/__init__.py
@@ -1,6 +1,1 @@
-# ==================================================================== #
-# File name: __init__.py
-# Author: Automation Lab - Sungkyunkwan University
-# Date created: 03/30/2021
-# ==================================================================== #
-
+from __future__ import annotations
