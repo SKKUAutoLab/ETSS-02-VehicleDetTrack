5289d9327d156d4432a6cd263e5adc9e6690bc8d
sugarubuntu <duongtran.k10@gmail.com>
2023-10-12 14:51:51 +0900

    Update TFE.

---
+++ src/tfe/run_inference.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Run each camera
+python main.py  \
+   --dataset carla_rain  \
+   --config Town10HD_location_6.yaml  \
+   --write_video True
+
+
+# Run all cameras
+#python main_all.py  \
+#   --dataset aicity2021_final \
+#   --write_video True
+
+# Run each camera with multi thread
+#python main_multithread.py  \
+#    --dataset aicity2021_final \
+#    --config cam_1.yaml
+
+# Run each camera with multi thread
+#python main_all_multithread.py  \
+#    --dataset aicity2021_final
+
+# Run each camera with multi process
+#python main_all_multiprocess.py  \
+#    --dataset aicity2021_final
+
+# Run each camera without detection
+#python main_non_detection.py  \
+#    --dataset carla  \
+#    --config Town10HD_location_1.yaml \
+#    --write_video True
+
+# Run each camera with multi process and  multi thread
+#python main_all_multiprocess_multithread.py  \
+#    --dataset aicity2021_final \
+#    --nodes 1
